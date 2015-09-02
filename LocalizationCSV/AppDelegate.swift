7020ff8a2765b547704f870d725bbeed869bd264
Rogelio Gudino <cananito@gmail.com>
2015-09-02 11:23:02 -0700

    Created the project.

---
+++ LocalizationCSV/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  LocalizationCSV
+//
+//  Created by Rogelio Gudino on 9/2/15.
+//  Copyright © 2015 Rogelio Gudino. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+    }
+}
---
+++ LocalizationCSV/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  LocalizationCSV
+//
+//  Created by Rogelio Gudino on 9/2/15.
+//  Copyright © 2015 Rogelio Gudino. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
