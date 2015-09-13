56bab6aa63994825ac0258ef6d6ffc5ec77b3dbb
Rogelio Gudino <cananito@gmail.com>
2015-09-12 20:45:49 -0700

    Added sqlte3.0 dependency and a bridging header to use it.

---
+++ LocalizationCSV/LocalizationCSV-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  LocalizationCSV-Bridging-Header.h
+//  LocalizationCSV
+//
+//  Created by Rogelio Gudino on 9/12/15.
+//  Copyright © 2015 Rogelio Gudino. All rights reserved.
+//
+
+#ifndef LocalizationCSV_Bridging_Header_h
+#define LocalizationCSV_Bridging_Header_h
+
+#import <sqlite3.h>
+
+#endif /* LocalizationCSV_Bridging_Header_h */
